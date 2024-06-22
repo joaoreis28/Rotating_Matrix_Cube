92eb01074fe9cc05f167dacc6480b9adc71d8601
Hippo Joao <reisjoaomendes@gmail.com>
2024-06-22 11:30:35 -0300

    Calculate Rotate coordinates.

--- main.c
+++ main.c
@@ -1,7 +1,41 @@
 #include <math.h>
 
 
+
+float A, B, C;
+
+float calcX(int i, int j, int k)
+{
+    return j* sin(A) * sin(B) * cos(C)
+           - k * cos(A) * sin(B) * cos(C)
+           + j * cos(A) * sin(C) 
+           + k * sin(A) * sin(C)
+           + i * cos(B) * cos(C);  
+}
+
+
+float calcY(int i, int j, int k)
+{
+    return j* cos(A) * cos(C)
+           + k * sin(A) * cos(C)
+           - j * sin(A) * sin(C) * sin(B) 
+           + k * cos(A) * sin(C) * sin(B)
+           - i * cos(B) * sin(C);  
+}
+
+
+float calcZ(int i, int j, int k)
+{
+    return k* cos(A) * cos(B)
+           - j * sin(A) * cos(B) 
+           + i * sin(B);
+}
+
+
+
+
+
 int main()
 {
-    
+    return 0;
 }
